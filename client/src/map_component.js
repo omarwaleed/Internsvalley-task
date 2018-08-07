23d2169950235d97fc0c8feb9be3d4fb3b8b1f6d
omarwaleed <omarpersonalprofile@gmail.com>
2018-08-07 21:12:54 +0200

    modified map component and finished all 3 main and the extra task remaining styling changes

--- client/src/map_component.js
+++ client/src/map_component.js
@@ -6,6 +6,8 @@ class MapComponent extends React.Component {
     this.state = {
       name: 'Loading...',
       coordinates: [0, 0],
+      marker: null,
+      distance: null
     };
   }
   componentDidMount() {
@@ -16,8 +18,17 @@ class MapComponent extends React.Component {
         this.setState({
           name: properties.name,
           coordinates: geometry.coordinates,
+        }, ()=>{
+          console.log(this.state.coordinates)
+          let coords = this.state.coordinates;
+          coords.reverse();
+          let marker = L.marker(coords).addTo(this.map);
+          this.setState({marker})
         });
-    });
+      })
+      .catch((err)=>{
+        console.error(err);
+      })
     this.map = new L.Map('mapid');
     const osmUrl='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
     const attribution = 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
@@ -27,12 +38,40 @@ class MapComponent extends React.Component {
       attribution,
     });
     this.map.setView(new L.LatLng(52.51, 13.40), 9);
+    // L.marker([52.51, 13.40]).addTo(this.map);
     this.map.addLayer(osm);
+    // console.log(this.map)
+    this.map.on('click', (event)=>{
+      console.log(event.latlng);
+      fetch(`http://localhost:3000/?lat=${event.latlng.lat}&lng=${event.latlng.lng}`)
+      .then((response)=>{
+        return response.json();
+      })
+      .then((json)=>{
+        console.log(json)
+        let coords = json.geometry.coordinates;
+        coords.reverse();
+        this.map.removeLayer(this.state.marker)
+        let marker = L.marker(coords).addTo(this.map);
+        this.setState({marker, distance: json.distance.toFixed(2), name: json.properties.name, coordinates: json.geometry.coordinates});
+      })
+      .catch((err)=>{
+        console.error(err);
+      })
+    })
   }
   render() {
     return (
         <div>
           Name: {this.state.name}<br />
+          {
+            this.state.distance?
+            <div>
+              Distance: {this.state.distance} Meters
+            </div>
+            :
+            null
+          }
           {this.state.coordinates[1]} {this.state.coordinates[0]}
           <div id="mapid"></div>
         </div>);
